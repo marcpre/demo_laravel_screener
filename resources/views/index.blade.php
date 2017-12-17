22fb811fa36d214c6cede50462f1916a92bafd17
marcpre <marcus.presich@gmail.com>
2017-12-17 10:43:42 +0100

    creating frontend for screener

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,130 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
+@extends('layouts.app') @section('content')
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Screener</div>
 
                 <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                    @endif {{-- Dropdowns Start --}}
+                    <ul class="nav nav-pills" role="tablist">
+                        <li role="presentation">Order: </li>
+                        <li role="presentation" class="dropdown">
+                            <a href="#" class="dropdown-toggle" id="drop4" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                Ticker
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" id="menu1" aria-labelledby="drop4">
+                                <li>
+                                    <a href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Something else here</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="#">Separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li role="presentation" class="dropdown">
+                            <a href="#" class="dropdown-toggle" id="drop5" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                ASC
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" id="menu2" aria-labelledby="drop5">
+                                <li>
+                                    <a href="#">ASC</a>
+                                </li>
+                                <li>
+                                    <a href="#">DESC</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li role="presentation">Signal: </li>
+                        <li role="presentation" class="dropdown">
+                            <a href="#" class="dropdown-toggle" id="drop6" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" id="menu3" aria-labelledby="drop6">
+                                <li>
+                                    <a href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Something else here</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="#">Separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li role="presentation">Tickers: </li>
+                        <li role="presentation" class="dropdown">
+                            <a href="#" class="dropdown-toggle" id="drop6" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" id="menu3" aria-labelledby="drop6">
+                                <li>
+                                    <a href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Something else here</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="#">Separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    {{-- Dropdowns Ende --}}
 
-                    You are logged in!
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <link href="{{ asset('css/mycss.css') }}" rel="stylesheet">    
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -75,6 +76,8 @@
     </div>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/dropdown.js') }}"></script>    
     <script src="{{ asset('js/app.js') }}"></script>
+ 
 </body>
 </html>
