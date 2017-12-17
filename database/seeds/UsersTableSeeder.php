99824e24131bf2a6d044ab6a952ff3cac42ea424
marcpre <marcus.presich@gmail.com>
2017-12-17 08:32:29 +0100

    init und seed users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+               
+        $user = new User();
+        $user->name = 'user';
+        $user->email = 'user@user.com';
+        $user->password = bcrypt('user');
+        $user->save();
+        
+        $admin = new User();
+        $admin->name = 'admin';
+        $admin->email = 'admin@admin.com';
+        $admin->password = bcrypt('admin');
+        $admin->save();
+    }
+}
