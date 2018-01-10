3e5a5d03f1353db93197e78b4a2beca28ed01835
marcpre <marcus.presich@gmail.com>
2018-01-10 06:51:19 +0000

    fix label styling

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -66,6 +66,10 @@
                                     </h3>
                                     <!-- Form -->
                                     <form action="echo.php" class="repeater" enctype="multipart/form-data">
+                                          <label>Name</label>
+                                          <label>Twitter</label>
+                                          <label>Twitter</label>
+                                          <label>Twitter</label>
                                         <div data-repeater-list="group-a">
                                             <div data-repeater-item>
                                                 <input name="untyped-input" value="A" />
@@ -87,6 +91,8 @@
                                     </h3>
                                     <!-- Form -->
                                     <form action="echo.php" class="repeater" enctype="multipart/form-data">
+                                          <label>Event Name</label>
+                                          <label>Link</label>
                                         <div data-repeater-list="group-a">
                                             <div data-repeater-item>
                                                 <input name="untyped-input" value="A" />
@@ -106,33 +112,6 @@
                                                 <select name="select-input">
                                                     <option value="A" selected>A</option>
                                                     <option value="B">B</option>
-                                                </select>
-
-                                                <select name="multiple-select-input" multiple>
-                                                    <option value="A" selected>A</option>
-                                                    <option value="B" selected>B</option>
-                                                </select>
-
-                                                <input data-repeater-delete type="button" value="Delete" />
-                                            </div>
-                                            <div data-repeater-item>
-                                                <input name="untyped-input" value="A" />
-
-                                                <input type="text" name="text-input" value="B" />
-
-                                                <input type="date" name="date-input" />
-
-                                                <textarea name="textarea-input">B</textarea>
-
-                                                <input type="radio" name="radio-input" value="A" />
-                                                <input type="radio" name="radio-input" value="B" checked/>
-
-                                                <input type="checkbox" name="checkbox-input" value="A" />
-                                                <input type="checkbox" name="checkbox-input" value="B" checked/>
-
-                                                <select name="select-input">
-                                                    <option value="A">A</option>
-                                                    <option value="B" selected>B</option>
                                                 </select>
 
                                                 <select name="multiple-select-input" multiple>
@@ -151,6 +130,7 @@
                                     </h3>
                                     <!-- Form -->
                                     <form action="echo.php" class="repeater" enctype="multipart/form-data">
+                                          <label>Link</label>
                                         <div data-repeater-list="group-a">
                                             <div data-repeater-item>
                                                 <input name="untyped-input" value="A" />
@@ -158,51 +138,6 @@
                                                 <input type="text" name="text-input" value="A" />
 
                                                 <input type="date" name="date-input" />
-
-                                                <textarea name="textarea-input">A</textarea>
-
-                                                <input type="radio" name="radio-input" value="A" checked/>
-                                                <input type="radio" name="radio-input" value="B" />
-
-                                                <input type="checkbox" name="checkbox-input" value="A" checked/>
-                                                <input type="checkbox" name="checkbox-input" value="B" />
-
-                                                <select name="select-input">
-                                                    <option value="A" selected>A</option>
-                                                    <option value="B">B</option>
-                                                </select>
-
-                                                <select name="multiple-select-input" multiple>
-                                                    <option value="A" selected>A</option>
-                                                    <option value="B" selected>B</option>
-                                                </select>
-
-                                                <input data-repeater-delete type="button" value="Delete" />
-                                            </div>
-                                            <div data-repeater-item>
-                                                <input name="untyped-input" value="A" />
-
-                                                <input type="text" name="text-input" value="B" />
-
-                                                <input type="date" name="date-input" />
-
-                                                <textarea name="textarea-input">B</textarea>
-
-                                                <input type="radio" name="radio-input" value="A" />
-                                                <input type="radio" name="radio-input" value="B" checked/>
-
-                                                <input type="checkbox" name="checkbox-input" value="A" />
-                                                <input type="checkbox" name="checkbox-input" value="B" checked/>
-
-                                                <select name="select-input">
-                                                    <option value="A">A</option>
-                                                    <option value="B" selected>B</option>
-                                                </select>
-
-                                                <select name="multiple-select-input" multiple>
-                                                    <option value="A" selected>A</option>
-                                                    <option value="B" selected>B</option>
-                                                </select>
 
                                                 <input data-repeater-delete type="button" value="Delete" />
                                             </div>
